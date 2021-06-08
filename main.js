b3a7934567c7437f3ac62634fa75c114170a9815
avanisrivastava <78412779+avanisrivastava@users.noreply.github.com>
2021-06-08 20:06:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array )
+    {
+        i = 0;
+    }
+    var updatedImage = images[i];
+    var updatedName  = names[i];
+   
+
+                document.getElementById("names").innerHTML=names[i];
+                document.getElementById("family_pic").innerHTML=family_pic;
+     
+                }
